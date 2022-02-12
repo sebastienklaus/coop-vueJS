85956ba72181cae70b39790900ca8827906dec13
sebastienklaus <sebastienklaus7@gmail.com>
2022-02-12 23:08:17 +0100

    add a h3 underlined

--- src/views/Membre.vue
+++ src/views/Membre.vue
@@ -11,7 +11,8 @@
       <div class="box has-background-link-light">
         <div class="content p-4" :key="membre.id" v-for="membre in filterUser">
           <h3 class="title is-3 has-text-centered">Profil de {{membre.fullname}}</h3>
-
+          
+          <h3 class="is-underlined">Informations sur l'utilisateur :</h3>
           <p><i class="far fa-clock"></i> Inscris le {{membre.created_at}}</p>
           <p>
             <i class="far fa-envelope"></i>
@@ -21,6 +22,7 @@
       </div>
     </section>
   </div>
+
 </template>
 
 <script>
