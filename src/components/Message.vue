9cfb6b224fb597a69ee37c498eaaf4d6cc6fc2f9
sebastienklaus <sebastienklaus7@gmail.com>
2022-02-19 23:05:32 +0100

    add created at

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -2,11 +2,6 @@
     <div class="card m-3">
         <div class="card-content">
             <div class="media">
-                <div class="media-left">
-                    <figure class="image is-48x48">
-                        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                    </figure>
-                </div>
                 <div class="media-content">
                     <p class="title is-4">
                         <router-link :to="'/membre/' + member.id">
@@ -18,6 +13,7 @@
             </div>
             <div class="content" :data-id-message="message.id">
                <p>{{ message.message }}</p>
+               <p>Envoyé le {{ message.created_at }}</p>
             </div>
         </div>
         <footer class="card-footer">
